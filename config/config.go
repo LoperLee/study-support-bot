fc73ce81c518608288a43bdfb5d7b43fc342969a
LoperLee <mds3512@naver.com>
2020-07-09 21:41:58 +0900

    Add config func

---
+++ config/config.go
@@ -0,0 +1,48 @@
+// This package is config loading tools
+package config
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+// YAML struct
+type Config struct {
+	Database struct {
+		Addr string `yarm: addr`
+		Port int    `yarm: port`
+		User string `yarm: user`
+		Pass string `yarm: pass`
+	} `yarm: Database`
+	Slack struct {
+		Token string `yarm: token`
+	} `yarm: Slack`
+}
+
+func LoadConfigration(path string) Config {
+	var config Config
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	err = yaml.Unmarshal(bytes, &config)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	return config
+}
+
+// func LoadConfiguration() Config {
+// 	var config Config
+// 	configFile, err := os.Open("config.json")
+// 	defer configFile.Close()
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	jsonParser := json.NewDecoder(configFile)
+// 	jsonParser.Decode(&config)
+// 	return config
+// }
