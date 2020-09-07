9b1a8b30416da9ce7a5d3de73be782d0f9b842bd
LoperLee <mds3512@naver.com>
2020-09-07 22:04:03 +0900

    New funcs

--- bot/slack.go
+++ bot/slack.go
@@ -3,9 +3,21 @@ package bot
 
 import "github.com/slack-go/slack"
 
+// save bot token struct
 type Bot struct {
 	apiKey *slack.Client
 	status int
+}
+
+// all message builder interface
+type Message interface {
+	Build() (string, slack.Attachment)
+	SetMessage()
+	SetAttach()
+}
+type Commit struct {
+	Message string
+	Attach  slack.Attachment
 }
 
 const (
@@ -21,4 +33,14 @@ func InitBot(key string) *Bot {
 		apiKey: slack.New(key),
 		status: NOT_LOADING,
 	}
+}
+
+// message to channel
+func (bot *Bot) SendingToChannel(message Message) {
+
+}
+
+// build Check Message
+func (com Commit) SetMessage(message string) {
+	com.Message = message
 }
